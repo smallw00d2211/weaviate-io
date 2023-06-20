8913ea40d1dece6200dba681932e360301bfd08a
Dan Dascalescu <dan@weaviate.io>
2023-06-20 08:41:42 -0500

    [DRAFT] Replace Giscus with DiscourseEmbed for comments

---
+++ src/components/DiscourseEmbed/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+export function DiscourseEmbed() {
+  useEffect(() => {
+    window.DiscourseEmbed = {
+      discourseUrl: 'https://forum.weaviate.io/',
+    };
+
+    const d = document.createElement('script');
+    d.type = 'text/javascript';
+    d.async = true;
+    d.src = window.DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
+  }, []);
+
+  return (
+    <div>
+      <div id="discourse-comments"></div>
+    </div>
+  );
+}
